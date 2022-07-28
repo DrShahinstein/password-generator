fceb11df378183c78dc427d72db61628c07e5495
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-07-28 12:25:03 +0300

    Arrange the password generation function within CLI system

--- password_generator/__init__.py
+++ password_generator/__init__.py
@@ -1,10 +1,18 @@
 import click
+from .generation import generate_password
 
-@click.group()
-def cli(): pass
+@click.command()
+@click.option('--length', default=8, help="Determine the length of the password | DEFAULT=8")
+@click.option('--upper', default=True, help="Enable/Disable uppercase chars (ABC) | DEFAULT=True")
+@click.option('--lower', default=True, help="Enable/Disable lowercase chars (abc) | DEFAULT=True")
+@click.option('--numeric', default=True, help="Enable/Disable numeric chars (123) | Default=True")
+@click.option('--punct', default=True, help="Enable/Disable punctuation chars (@!?) | Default=True")
 
-def main():
-    cli()
+def main(length, upper, lower, numeric, punct):
+    """Generate a Strong Password"""
+    
+    password = generate_password(length, numeric, lower, upper, punct)
+    return password
 
 if __name__ == "__main__":
     main()
