07954f35d756c8e28479448d067cf036bcc4bf82
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-07-28 10:20:12 +0300

    Get rid of unnecessary condition

--- password_generator/generation.py
+++ password_generator/generation.py
@@ -1,19 +1,19 @@
 import string 
 import random
 
-def generate_password(length: int, alphabetic=True, numeric=True, lowercase=True, uppercase=True, special_chars=True) -> str:
+def generate_password(length: int, numeric=True, lowercase=True, uppercase=True, special_chars=True) -> str:
     chars = []
-    if alphabetic:
-        if lowercase:
-            chars += [*string.ascii_lowercase]
-        if uppercase:
-            chars += [*string.ascii_uppercase]
+    
+    if lowercase:
+        chars += [*string.ascii_lowercase]
+    if uppercase:
+        chars += [*string.ascii_uppercase]
     if numeric:
         chars += [*string.digits]
     if special_chars:
         chars += [*string.punctuation]   
+        
     random.shuffle(chars)
-    
     return "".join([chars[i] for i in range(length)])
         
 
