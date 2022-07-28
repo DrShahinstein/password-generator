a9f7c350e37bb9736b82d9270b40288ff16ce4ee
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-07-28 12:53:18 +0300

    Create a click group, rename generate command and fix printing the generated password.

--- password_generator/__init__.py
+++ password_generator/__init__.py
@@ -1,18 +1,24 @@
 import click
 from .generation import generate_password
 
-@click.command()
+@click.group()
+def cli(): pass
+
+@cli.command()
 @click.option('--length', default=8, help="Determine the length of the password | DEFAULT=8")
 @click.option('--upper', default=True, help="Enable/Disable uppercase chars (ABC) | DEFAULT=True")
 @click.option('--lower', default=True, help="Enable/Disable lowercase chars (abc) | DEFAULT=True")
 @click.option('--numeric', default=True, help="Enable/Disable numeric chars (123) | Default=True")
 @click.option('--punct', default=True, help="Enable/Disable punctuation chars (@!?) | Default=True")
 
-def main(length, upper, lower, numeric, punct):
+def generate(length, upper, lower, numeric, punct):
     """Generate a Strong Password"""
     
     password = generate_password(length, numeric, lower, upper, punct)
-    return password
+    click.echo(password)
+
+def main():
+    cli()
 
 if __name__ == "__main__":
     main()
