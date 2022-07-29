f81ab4af11a52308fa7feaf01a10c91d69ca5ec6
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-07-29 12:16:30 +0300

    Complete the save command structurally ✅

--- password_generator/__init__.py
+++ password_generator/__init__.py
@@ -1,8 +1,10 @@
 import click
 from .generation import generate_password
 
+
 @click.group()
 def cli(): pass
+
 
 @cli.command()
 @click.option('--length', default=8, help="Determine the length of the password | DEFAULT=8")
@@ -12,18 +14,34 @@ def cli(): pass
 @click.option('--punct', default=True, help="Enable/Disable punctuation chars (@!?) | Default=True")
 def generate(length, upper, lower, numeric, punct):
     """Generate a Strong Password"""
-    
+
     password = generate_password(length, numeric, lower, upper, punct)
     click.echo(password)
 
+
 @cli.command()
-@click.option("--vault", help="Pick a vault to put your password in (business, social_media, etc.)")
+@click.option("--vault", default="default", help="Pick a vault to put your password in (business, social_media, etc.)")
 @click.argument("password_identifier")
-def save(password_identifier):
-    pass
+def save(vault, password_identifier):
+    """Save your passwords into your vaults"""
+    
+    vault_password = click.prompt(f"Enter your vault password for your vault {vault}", hide_input=True)
+    password = click.prompt("Enter your password", hide_input=True)
+    password_repetition = click.prompt("Enter your password again", hide_input=True)
+    
+    # The following lines of codes are in test/debug purpose
+    if password == password_repetition:
+        click.echo("Password has been saved!")
+        click.echo(f"You've written {vault_password} for your vault password unlocking your vault")
+        click.echo(f"{password} is your password")
+        click.echo(f"{password_identifier} is your password_identifier")
+    else:
+        click.echo("Please make sure you did write the same passwords")
+
 
 def main():
     cli()
+
 
 if __name__ == "__main__":
     main()
